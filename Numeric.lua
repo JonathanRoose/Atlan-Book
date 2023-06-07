cd9f8d23feb367af81cd56f9a5bb6927a23f4d9b
Jarno Smets <jarnosmets02@gmail.com>
2023-06-07 14:39:13 +0200

    commit june 7 14:39

--- Numeric.lua
+++ Numeric.lua
@@ -68,4 +68,110 @@ function Cisterian(number)
 	return result	
 end
 
+function Cisteriandd(number)
+        --Getting the individual digits from a larger digit
+        for i=1,9 do 
+                if number >= 1000 then firstdigit = math.floor(number/1000)  
+                numbersecond = number - firstdigit * 1000 
+                else firstdigit = " "
+                numbersecond = number
+                end 
+                if number >= 100  then seconddigit = math.floor(numbersecond/100)  
+                numberthird = numbersecond - seconddigit * 100
+                else seconddigit = " " 
+                numberthird = numbersecond
+                end 
+                if number >= 10  then thirddigit = math.floor(numberthird/10)
+                numberfourth = numberthird - thirddigit * 10
+                else thirddigit = " " 
+                numberfourth = numberthird
+                end 
+                if number >= 1  then fourthdigit = math.floor(numberfourth/1)  else fourthdigit = " " end 
+	end
+	print(firstdigit)
+	print(seconddigit)
+	print(thirddigit)
+	print(fourthdigit)
+	bignumber = tonumber(firstdigit .. seconddigit .. thirddigit .. fourthdigit)
+	print(bignumber)
+	if bignumber >= 1000 then ddfirstdigit = math.floor(bignumber/1728)
+	bignumbersecond = bignumber - (math.floor(bignumber/1728) * 1728)  
+	else ddfirstdigit = " "
+	bignumbersecond = bignumber
+	end 
+	if bignumber >= 100  then ddseconddigit = math.floor(bignumbersecond/144)  
+	bignumberthird = bignumbersecond - (math.floor(bignumbersecond/144) * 144)   
+	else ddseconddigit = " " 
+	bignumberthird = bignumbersecond
+	end 
+	if bignumber >= 10  then ddthirddigit = math.floor(bignumberthird/12)
+	bignumberfourth = bignumberthird - (math.floor(bignumberthird/12) * 12)
+	else ddthirddigit = " " 
+	bignumberfourth = bignumberthird
+	end 
+	if bignumber >= 1  then ddfourthdigit = math.floor(bignumberfourth/1)  else ddfourthdigit = " " end 
+	print(ddfirstdigit)
+	print(ddseconddigit)
+	print(ddthirddigit)
+	print(ddfourthdigit)
+
+
+
+if ddfirstdigit == 1 then firstargument ="\\quadrupleone"
+	elseif ddfirstdigit == 2 then firstargument ="\\quadrupletwo"
+	elseif ddfirstdigit == 3 then firstargument ="\\quadruplethree"
+	elseif ddfirstdigit == 4 then firstargument ="\\quadruplefour"
+	elseif ddfirstdigit == 5 then firstargument ="\\quadruplefive"
+	elseif ddfirstdigit == 6 then firstargument ="\\quadruplesix"
+	elseif ddfirstdigit == 7 then firstargument ="\\quadrupleseven"
+	elseif ddfirstdigit == 8 then firstargument ="\\quadrupleeight"
+	elseif ddfirstdigit == 9 then firstargument ="\\quadruplenine"
+	elseif ddfirstdigit == 10 then firstargument = "\\quadrupleddten"
+	elseif ddfirstdigit == 11 then firstargument = "\\quadrupleddeleven"
+	else firstargument = "\\nil"
+	end
+	if ddseconddigit == 1 then secondargument ="\\tripleone"
+	elseif ddseconddigit == 2 then secondargument ="\\tripletwo"
+	elseif ddseconddigit == 3 then secondargument ="\\triplethree"
+	elseif ddseconddigit == 4 then secondargument ="\\triplefour"
+	elseif ddseconddigit == 5 then secondargument ="\\triplefive"
+	elseif ddseconddigit == 6 then secondargument ="\\triplesix"
+	elseif ddseconddigit == 7 then secondargument ="\\tripleseven"
+	elseif ddseconddigit == 8 then secondargument ="\\tripleeight"
+	elseif ddseconddigit == 9 then secondargument ="\\triplenine"
+	elseif ddseconddigit == 10 then secondargument = "\\tripleddten"
+	elseif ddseconddigit == 11 then secondargument = "\\tripleddeleven"
+	else secondargument = "\\nil"
+	end
+	if ddthirddigit == 1 then thirdargument ="\\doubleone"
+	elseif ddthirddigit == 2 then thirdargument ="\\doubletwo"
+	elseif ddthirddigit == 3 then thirdargument ="\\doublethree"
+	elseif ddthirddigit == 4 then thirdargument ="\\doublefour"
+	elseif ddthirddigit == 5 then thirdargument ="\\doublefive"
+	elseif ddthirddigit == 6 then thirdargument ="\\doublesix"
+	elseif ddthirddigit == 7 then thirdargument ="\\doubleseven"
+	elseif ddthirddigit == 8 then thirdargument ="\\doubleeight"
+	elseif ddthirddigit == 9 then thirdargument ="\\doublenine"
+	elseif ddthirddigit == 10 then thirdargument = "\\doubleddten"
+	elseif ddthirddigit == 11 then thirdargument = "\\doubleddeleven"
+	else thirdargument  = "\\nil"
+	end
+	if ddfourthdigit == 1 then fourthargument ="\\singleone"
+	elseif ddfourthdigit == 2 then fourthargument ="\\singletwo"
+	elseif ddfourthdigit == 3 then fourthargument ="\\singlethree"
+	elseif ddfourthdigit == 4 then fourthargument ="\\singlefour"
+	elseif ddfourthdigit == 5 then fourthargument ="\\singlefive"
+	elseif ddfourthdigit == 6 then fourthargument ="\\singlesix"
+	elseif ddfourthdigit == 7 then fourthargument ="\\singleseven"
+	elseif ddfourthdigit == 8 then fourthargument ="\\singleeight"
+	elseif ddfourthdigit == 9 then fourthargument ="\\singlenine"
+	elseif ddfourthdigit == 10 then fourthargument = "\\singleddten"
+	elseif ddfourthdigit == 11 then fourthargument = "\\singleddeleven"
+	else fourthargument = "\\nil"
+	end
+	result = "\\DeclareNumber{" .. firstargument .. "}{" .. secondargument .. "}{" .. thirdargument .. "}{" .. fourthargument .. "}"
+	return result	
+end
+print(Cisteriandd(1727))
+
 
